c20ca1d67f8d3758324abb287fd35546090dc63a
schAKura <alan.kuranowski@tm1.edu.pl>
2024-09-30 13:20:38 +0200

    Changes to be committed: modified:   PKNozyce/index.html modified:   PKNozyce/script.js

--- PKNozyce/script.js
+++ PKNozyce/script.js
@@ -1,1 +1,21 @@
+class Mozliwosc {
+    constructor(name1, image1) {
+        this.name = name1;
+        this.img = image1;
+    }
+}
 
+const mozliwosci = [
+    new Mozliwosc("Papier", "media/papier.png"),
+    new Mozliwosc("Kamien", "media/kamien.png"),
+    new Mozliwosc("Nozyce", "media/nozyce.png")
+]
+
+
+function losowanie(){
+    let a = Math.floor(Math.random() * mozliwosci.length)
+    return mozliwosci[a]
+}
+
+let odp = losowanie()
+document.write(odp.name + "<img src=" + odp.img + "></img>")
